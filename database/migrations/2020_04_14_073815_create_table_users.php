850da2b79135e5ebe011bd651bfaad1d54a684a7
sashenme <sashenpasindu@gmail.com>
2020-04-14 15:47:08 +0530

    initial commit

---
+++ database/migrations/2020_04_14_073815_create_table_users.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableUsers extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('users', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->ipAddress('ip');
+            $table->integer('emp_id')->nullable();
+            $table->integer('division');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('users');
+    }
+}
