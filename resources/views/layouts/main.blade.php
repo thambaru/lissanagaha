e9ec20d2f2ebb1c18399145ed8a4dea11c4a70f3
sashenme <sashenpasindu@gmail.com>
2020-04-16 03:00:17 +0530

    Add logo

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -12,6 +12,7 @@
 
 <body>
     <div class="container-fluid main-container">
+        <img src="{{asset('img/logo.png')}}" alt="">
           @yield('content')
     </div>
     <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
